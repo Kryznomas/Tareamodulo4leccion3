e72ad2e0193c58fb4cf583fe39e92104b7114f26
Kryznomas <kryztop@gmail.com>
2021-03-03 17:56:40 -0300

    intento de insertar tarjetas en modal

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,37 +1,76 @@
 Vue.component('carrusel',{
 
     template:
-    `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner" >
-            <div v-for="imagen in imagenes" class="carousel-item" v-bind:class="imagen.clase">
-                <img v-bind:src="imagen.src" class="d-block w-100">
+    `<div>
+        <div class="modal fade" data-bs-target="#myModal" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl  ">
+                <div class="modal-content bg-dark bg-gradient">
+                    <div class="modal-header">
+                        <h5 class="text-light">{{label2}} </h5>
+                        <select class="form-select bg-secondary text-light" aria-label="Default select example">
+                            <option selected>{{label}}</option>
+                            <option value="1">{{option1}}</option>
+                            <option value="2">{{option2}}</option>
+                        </select>
+                    </div>
+                    <div class="modal-body text-light">
+                        <div id="targetModal"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">        
+            <div class="carousel-inner" >
+                <div v-for="(imagen, index) in imagenes" class="carousel-item" v-bind:class="imagen.clase">
+                    <img @click="datoModal(index)" :data-bs-toggle="isDisabled" :data-bs-target="idModal" v-bind:src="imagen.src" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </div>`,
     
     data(){
         return {
-        imagenes: [
+            isDisabled: 'modal',
+            idModal: '#myModal',
+            label: "Seleccione qué tipo de propiedad busca",
+            label2: "¿Arriendo o Venta?",
+            option1: "Arriendo",
+            option2: "Venta",
             
-            {src: '/assets/img/cabañamontaña1.jpg',
-            clase: "active"},
-            
-            {src: '/assets/img/deptosportada.jpg',
-            clase: ""},
-            
-            {src: '/assets/img/casacampoportada.jpg',
-            clase: ""},
-            
-            {src: '/assets/img/casaplaya1.jpg',
-            clase: ""},
-        ],
-    }},
+            imagenes: [
+                
+                {src: '/assets/img/cabañamontaña1.jpg',
+                clase: "active",
+                tarjetas: "<tarjetasmontana></tarjetasmontana>"},
+                
+                {src: '/assets/img/deptosportada.jpg',
+                clase: "",
+                tarjetas: "<tarjetasdeptos></tarjetasdeptos>"},
+                
+                {src: '/assets/img/casacampoportada.jpg',
+                clase: "",
+                tarjetas: "<tarjetascampo></tarjetascampo>"},
+                
+                {src: '/assets/img/casaplaya1.jpg',
+                clase: "",
+                tarjetas: "<tarjetasplaya></tarjetasmontana>"},
+            ],
+        }},
+        methods: {
+            datoModal(index) {
+                el = document.getElementById('targetModal');
+                el.innerHTML = this.imagenes[index].tarjetas
+            }
+        }
 })
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,27 +3,40 @@ Vue.component('modal',{
     template:
     `
     <div>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-        Launch static backdrop modal
-    </button>
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal fade" data-bs-target="#myModal" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl  ">
         <div class="modal-content bg-dark bg-gradient">
             <div class="modal-header">
-                <select class="form-select bg-secondary text-light" aria-label="Default select example">
-                    <option selected></option>
-                    <option value="1"></option>
-                    <option value="2"></option>
-                </select>
+                <h3 style="color:white">Elija su destino </h3>
             </div>
             <div class="modal-body text-light">
+            <p id="targetModal"></p>
             </div>
             <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             </div>
         </div>
         </div>
-    </div>
+        <div>
+            <div class="modal fade" data-bs-target="#myModal" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl  ">
+                <div class="modal-content bg-dark bg-gradient">
+                    <div class="modal-header">
+                        <select class="form-select bg-secondary text-light" aria-label="Default select example">
+                            <option selected></option>
+                            <option value="1"></option>
+                            <option value="2"></option>
+                        </select>
+                    </div>
+                    <div class="modal-body text-light">
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
     `
     ,
